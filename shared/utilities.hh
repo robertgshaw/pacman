29077aaf7c168edad8983cf60e3d9cfc149a50d5
CS61 User <nobody@example.com>
2021-05-08 15:56:21 -0400

    updated gitignore to not track .o files

--- shared/utilities.hh
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <sys/socket.h> // socket
-#include <arpa/inet.h>	// inet_addr
-#include <unistd.h>
-
-std::string parse_message(char* server_msg, const char* format, const char* b_format, int b_kw_len);
